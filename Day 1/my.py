eedf005aa09d61ccb49c3e6589d028121b4a90ee
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-17 09:40:34 +0000

    Add my.py to all day folders

--- Day 1/my.py
+++ Day 1/my.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print("Hello")
-=======
 print("Hello") 
 name = input("What is your name? ")
 print("Hello, " + name + "!")
@@ -10,6 +7,5 @@ sum = num1 + num2
 print("The sum is:", sum)
 name = input("What is your name? ")
 print("Hello, " + name + "!")
-color = input("What is your favorite color? " )
+color = input("What is your favorite color?")
 print("Your favorite color is ", color) 
->>>>>>> 34d362b (Add Day 1 exercises)
