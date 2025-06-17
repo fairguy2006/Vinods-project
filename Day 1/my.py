066af4a80b6800107f0759fe8aa0b4469368254d
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-17 12:38:23 +0000

    Done with Day 1 and Day 2 exercises

--- Day 1/my.py
+++ Day 1/my.py
@@ -1,11 +1,13 @@
 print("Hello") 
 name = input("What is your name? ")
 print("Hello, " + name + "!")
+age = input("How old are you? ")
+print("Your name is " + name + " and you are " + age + " years old.")
+color = input("What is your favorite color?")
+print("Your favorite color is ", color) 
+# Simple addition example
 num1 = 5
 num2 = 3
 sum = num1 + num2
 print("The sum is:", sum)
-name = input("What is your name? ")
-print("Hello, " + name + "!")
-color = input("What is your favorite color?")
-print("Your favorite color is ", color) 
+
---
+++ Day 2 /my.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum is:", sum)
+# Check whether the user is an adult or a minor
+age = int(input("How old are you?"))
+if age >= 18:
+     print("You are an adult.")
+else:
+     print("You are a minor.")
+
+
