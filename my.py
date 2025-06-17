f72d96a266920f846fdd028c47a4e58d8540040d
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-17 08:56:09 +0000

    Add Day 1 exercises

--- my.py
+++ my.py
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
 print("Hello")
+=======
+print("Hello") 
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+num1 = 5
+num2 = 3
+sum = num1 + num2
+print("The sum is:", sum)
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+color = input("What is your favorite color? " )
+print("Your favorite color is ", color) 
+>>>>>>> 34d362b (Add Day 1 exercises)
