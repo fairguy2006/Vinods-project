ca127bcc12884824790e50a044b76b7c4c69207e
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-17 13:18:30 +0000

    Learnt to print a list of fruits

---
+++ Day 3/my.py
@@ -0,0 +1,10 @@
+fruits =["apple" , "banana" , "orange" , "kiwi" , "mango"]
+print("Here is a list of fruits:", fruits)
+fruits.append("grape")
+print("Now the list is:", fruits)
+print("let's print each fruit:")
+for fruit in fruits:
+    print(fruit)
+
+
+
