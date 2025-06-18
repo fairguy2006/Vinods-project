b1f9dbc85aa4c962de5faf8282f08e247fe977b2
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-18 04:19:26 +0000

    day 3

--- Day 3/my.py
+++ Day 3/my.py
@@ -1,10 +1,16 @@
+# Create and Print a List 
 fruits =["apple" , "banana" , "orange" , "kiwi" , "mango"]
+#Print the list
 print("Here is a list of fruits:", fruits)
+#Added a new fruit
 fruits.append("grape")
+#Printed the updated list
 print("Now the list is:", fruits)
+#Looped through the list and printed each fruit
 print("let's print each fruit:")
 for fruit in fruits:
     print(fruit)
+
 
 
 
