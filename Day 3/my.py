30c574b31a544b9767fab68dfebec68d1a9defaa
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-18 04:36:09 +0000

    Day 3

--- Day 3/my.py
+++ Day 3/my.py
@@ -1,15 +1,26 @@
 # Create and Print a List 
 fruits =["apple" , "banana" , "orange" , "kiwi" , "mango"]
-#Print the list
+# Print the list
 print("Here is a list of fruits:", fruits)
-#Added a new fruit
+# Added a new fruit
 fruits.append("grape")
-#Printed the updated list
+# Printed the updated list
 print("Now the list is:", fruits)
-#Looped through the list and printed each fruit
+# Looped through the list and printed each fruit
 print("let's print each fruit:")
 for fruit in fruits:
     print(fruit)
+#  List Slicing
+fruits = ["apple' , "banana", "orange", "kiwi", "mango","grape"]    
+#get the first three fruits
+first_three = fruits[0:3]
+print("the first three fruits are:", first_three)
+# get the last two fruits
+last_two = fruits[-2:]
+print("The last to fruits are:", last_two)
+#  Find the Length of a List
+print("there are", len(fruits), "fruits in list")
+
 
 
 
