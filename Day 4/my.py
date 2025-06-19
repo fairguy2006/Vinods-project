3e8e0b45e52f49218377963ff62ec983367ab9e5
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-19 17:18:19 +0000

    Day 4 extra excercises

--- Day 4/my.py
+++ Day 4/my.py
@@ -8,11 +8,19 @@ print("The person lives in:", person["city"])
 # Add a New Key-Value Pair to the Dictionary
 person["occupation"] = "Engineer"
 person["hobby"] = "reading"
-print("Now the dictionary is:")
 print("After adding a new key-value pair, the dictionary is:")
 for key, value in person.items():
      print(key + ":", value)
 #Update a Value in the Dictionary
 person['age'] = 31
 print("updated age:" , person["age"])
-
+person["address"]= { "city" : "New Delhi", "Zip": "560084"} 
+for key, value in person.items():
+    print(key + ":", value)
+    # User's Favourite book and movie
+favorites = {}
+favorites["book"] = input ("what is your favourite book? ") # type: ignore
+favorites['movie'] = input("what is your favourite movie? ")
+print("Your favorites:")
+for key, value in favorites.items():
+    print(key.capitalize() + ":", value)
