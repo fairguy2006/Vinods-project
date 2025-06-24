1e55e61d1eb59f9d8cbfa68d77feb7143c7a1f1d
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-24 07:47:36 +0000

    Weather App Exc 1

--- Day 5/my.py
+++ Day 5/my.py
@@ -1,5 +1,17 @@
 import requests
-city =input("Enter the name of the city:")
-url = f"https://wttr.in/{city}?format=3"
-response = requests.get(url)
-print(response.text)
+
+cities = input("Enter city names separated by commas: ")
+city_list = [c.strip() for c in cities.split(",")]
+valid_count = 0
+invalid_count = 0
+for city in city_list:
+    url = f"https://wttr.in/{city}?format=3"
+    response = requests.get(url)
+    if "Unknown location" in response.text or "Sorry" in response.text:
+        print(f"{city}: Invalid city")
+        invalid_count += 1
+    else:
+        print(response.text)
+        valid_count += 1
+    print(f"\nvalid cities: {valid_count}")
+    print(f"Invalid cities: {invalid_count}")
