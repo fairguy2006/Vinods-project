db2a2b0dc649e3970df74f94df56d5779d7cb487
fairguy2006 <152473574+fairguy2006@users.noreply.github.com>
2025-06-24 09:26:09 +0000

    excercise2

---
+++ Day 5/excercise2 my.py
@@ -0,0 +1,21 @@
+import requests
+cities = input("Enter city name separated by commas: ")
+city_list = [c.strip() for c in cities.split(",")]
+valid_count = 0
+invalid_count = 0
+
+for city in city_list:
+    if not city: #skip empty city names
+         continue 
+    url = f"https://wttr.in/{city}?format=3"
+    response = requests.get(url)
+    if "Unknown location" in response.text or "Sorry" in response.text:
+        print(f"{city}:Invalid city")
+        invalid_count +=1
+    else:
+        print(response.text)
+        valid_count += 1
+
+print(f"\nValid cities: {valid_count}")
+print(f"Invalid cities: {invalid_count}")
+
